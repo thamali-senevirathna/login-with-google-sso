02c176be3d67aa855592f55470b2ea5929e67e92
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-29 16:20:01 +0530

    added two page

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-type LogProps={};
+type LogProps = {};
 
-const SignIn: React.FC<LogProps> = () =>{
-return (
+const SignIn: React.FC<LogProps> = () => {
+  return (
     <div>
-        <h1>Login</h1>
-        
+      <article
+        className="container col-md-6"
+        style={{
+          backgroundColor: "white",
+          position: "absolute",
+          width: "160vh",
+          height: "89vh",
+          left: "12%",
+          top: "5%",
+        }}
+      >
+        <div
+          className="row col-md-6"
+          style={{
+            position: "relative",
+            width: "52%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+          }}
+        ></div>
+      </article>
     </div>
-);
-}
+  );
+};
 
 export default SignIn;
