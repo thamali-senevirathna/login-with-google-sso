ed748068d9d2a7a1c62beecad2e8dfee3f2b6ad8
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-29 15:45:23 +0530

    install boostrap and import into the login component

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 type LogProps={};
 
 const SignIn: React.FC<LogProps> = () =>{
 return (
     <div>
         <h1>Login</h1>
+        
     </div>
 );
 }
