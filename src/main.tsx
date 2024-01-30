effb34192ca33e2b6a079eec977b1b9d5f60f1dd
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-30 19:25:58 +0530

    app component wrap brouserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}>
-    <App />
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
    
   </React.StrictMode>,
