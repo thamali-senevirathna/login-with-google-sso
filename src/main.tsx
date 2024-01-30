4304264d858faa46bb92f40a6fa055a786ee3310
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-30 19:23:43 +0530

    set google oauth api client id as a string

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}>
     <App />
+    </GoogleOAuthProvider>
+   
   </React.StrictMode>,
 )
